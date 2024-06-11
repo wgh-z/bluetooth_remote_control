3400900dbb4b492876b259600eb29f9c5780962b
wgh-12400 <3030649311@qq.com>
2024-06-11 13:16:55 +0800

    updated flask video play

--- control_end.py
+++ control_end.py
@@ -1,55 +1,89 @@
 import bluetooth
+import yaml
 import cv2
-from threading import Thread
-import numpy as np
 import pickle
-import time
-# import functools
+from flask import Flask, Response, jsonify, request
+from flask_cors import CORS
 
 
-# def screen_reader(camera=0, width=1920, height=1080, fps=30):
-#     cap = cv2.VideoCapture(camera)
-#     # 设置摄像头参数
-#     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-#     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-#     cap.set(cv2.CAP_PROP_FPS, fps)
-#     while True:
-#         ret, frame = cap.read()
-#         if not ret:
-#             break
-#         yield frame
+#开始主程序
+app = Flask(__name__)
 
-# def transfer_image(host, port, camera=0):
-#     sock = bluetooth.BluetoothSocket()
-#     sock.connect((host, port))
-#     print(f"已连接{host}:{port}")
+CORS(app, supports_credentials=True)
 
+def gen(cfg):
+    """视频流生成"""
+    camera = cfg['camera']
+    width = cfg['width']
+    height = cfg['height']
+    fps = cfg['fps']
 
-#     reader = screen_reader(camera)
-#     for frame in reader:
-#         print(frame.shape)
-#         cv2.imshow('frame', frame)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-#         im_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
-#         sock.send(im_bytes)
-#     sock.close()
+    cam = cv2.VideoCapture(camera)
+    # 设置摄像头参数
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    cam.set(cv2.CAP_PROP_FPS, fps)
 
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            break
+        frame = cv2.resize(frame, (1920, 1080))
+        data = cv2.imencode('.jpg', frame)[1]
+        frame = data.tobytes()
+        yield (b'--frame\r\n'
+                          b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-l_point, r_point = None, None
-camera = 0
-width = 1280
-height = 720
-fps = 60
-prior_event = None
-prior_flag = None
+@app.route('/video_feed')
+def video_feed():
+    global cfg
+    return Response(gen(cfg), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-cool_down_count = 20
-cool_down = cool_down_count
-# cv2.EVENT_LBUTTONDBLCLK
+@app.route('/dblclick', methods=['POST'])
+def dblclick():
+    global sock, cfg
+    width = cfg['width']
+    height = cfg['height']
 
-def send_controls(sock, event, x, y, width=1920, height=1080):
-    message = (event, x/width, y/height)
+    data = request.json['data']
+    x = int(float(data['x']) * 1920)
+    y = int(float(data['y']) * 1080)
+    send_controls(sock, 7, x, y, width, height)
+    return jsonify({
+      'code': 0
+    })
+
+@app.route('/click', methods=['POST'])
+def click():
+    global sock, cfg
+    width = cfg['width']
+    height = cfg['height']
+
+    data = request.json['data']
+    x = int(float(data['x']) * 1920)
+    y = int(float(data['y']) * 1080)
+    send_controls(sock, 3, x, y, width, height)
+    print(data)
+    return jsonify({
+      'code': 0
+    })
+
+@app.route('/input', methods=['POST'])
+def input():
+    global sock
+
+    data = request.json['data']
+    key = data['key']
+    send_controls(sock, event=key, keyboard=True)  # left, right, up, down, +, -
+    return jsonify({
+      'code': 0
+    })
+
+def send_controls(sock, event, x=None, y=None, width=1920, height=1080, keyboard=False):
+    if keyboard:
+        message = event
+    else:
+        message = (event, x, y, width, height)
     # print('message=', message)
     try:
         # message_bytes = np.array(message).tobytes()
@@ -61,82 +95,23 @@ def send_controls(sock, event, x, y, width=1920, height=1080):
     except Exception as e:
         print(e)
 
-def on_mouse(event, x, y, flags, param):
-    global l_point, r_point, sock, width, height, prior_event, cool_down, cool_down_count
-
-    # event falgs
-    #   0    0   :移动
-    #   1    1   :左键按下
-    #   4    0   :左键释放
-    #   7    1   :左键双击
-    #   2    2   :右键按下
-    #   5    0   :右键释放
-    #   8    2   :右键双击
-    #   10   7864320   :滚轮上滚
-    #   10   -7864320   :滚轮下滚
-    #   9    7864320   :滚轮上滚
-
-    print('event:', event, x, y, flags, param)
-    if event == 1 or event == 7:  # 左键按下
-        # print('左键按下')
-        send_controls(sock, 1, x, y, width, height)
-    elif event == 4:
-        # print('左键释放')
-        send_controls(sock, 3, x, y, width, height)
-    elif event == 2 or event == 8:  # 右键按下
-        # print('右键按下')
-        send_controls(sock, 2, x, y, width, height)
-    elif event == 5:
-        # print('右键释放')
-        send_controls(sock, 4, x, y, width, height)
-    elif event == 0:  # 移动
-        # print('移动')
-        if cool_down:
-            cool_down -= 1
-        else:
-            send_controls(sock, 0, x, y, width, height)
-            cool_down = cool_down_count
-        # time.sleep(0.1)
-
-
-def main(host, port, camera=0, width=1280, height=720, fps=60):
-    global l_point, r_point, sock
-
-    sock = bluetooth.BluetoothSocket()
-    sock.connect((host, port))
-    print(f"已连接{host}:{port}")
-
-    cv2.namedWindow('frame')
-    cv2.setMouseCallback('frame', on_mouse)
-
-    cam = cv2.VideoCapture(camera)
-    # 设置摄像头参数
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    cam.set(cv2.CAP_PROP_FPS, fps)
-    # ret = True
-    # frame = cv2.imread('test.jpg')
-    # frame = cv2.resize(frame, (width, height))
-    while True:
-        ret, frame = cam.read()
-        if not ret:
-            break
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            send_controls(sock, -1, 0, 0)
-            break
-    # cam.release()
-
 
 if __name__ == '__main__':
     # 读取蓝牙地址
-    with open('control_end.txt', 'r') as f:
-        bluetooth_host_addr = f.readline().strip()  # 远程蓝牙地址
-        bluetooth_port = int(f.readline().strip())  # 远程蓝牙端口
-        host = f.readline().strip()  # flask绑定ip
-        port = int(f.readline().strip())  # flask绑定端口
-
+    with open('control_end.yaml', 'r', encoding='utf-8') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
 
     # host = '00:A6:23:12:0F:DC'  # 远程蓝牙地址
     # 00:A6:23:12:0F:2C
-    main(bluetooth_host_addr, bluetooth_port, camera, width, height, fps)
+
+    bluetooth_ip = cfg['bluetooth_ip']
+    bluetooth_port = cfg['bluetooth_port']
+    camera = cfg['camera']
+    flask_ip = cfg['flask_ip']
+    flask_port = cfg['flask_port']
+
+    sock = bluetooth.BluetoothSocket()
+    sock.connect((bluetooth_ip, bluetooth_port))
+    print(f"蓝牙已连接{bluetooth_ip}:{bluetooth_port}")
+
+    app.run(host=flask_ip, port=flask_port, debug=True, threaded=True, processes=True)
